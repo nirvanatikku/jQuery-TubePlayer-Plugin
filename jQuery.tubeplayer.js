1aa41a8ff31c35a96cffd6fa514dc4dbeeb9975e
Nirvana Tikku <ntikku@gmail.com>
2013-01-17 14:17:08 -0500

    merge pull request from @mikelovesrobots to fix a null exception on destroy

--- jQuery.tubeplayer.js
+++ jQuery.tubeplayer.js
@@ -797,19 +797,16 @@
 			var d = $.tubeplayer.defaults;
 			
 			var events = ['unstarted','ended','playing','paused','buffering','cued'];
-			for(var _event in events) {
-				if (events.hasOwnProperty(_event)) {
-					delete d.onPlayer[events[_event]][p.opts.playerID];
-				}
-			}
-			
+
+			jQuery.each(events, function(i, event) {
+				delete d.onPlayer[event][p.opts.playerID];
+			});
+			    
 			events = ['notFound','notEmbeddable','invalidParameter'];
-			for(var _event in events) {
-				if (events.hasOwnProperty(_event)) {
-					delete d.onErr[events[_event]][p.opts.playerID];
-				}
-			}
-			
+			jQuery.each(events, function(i, event) {
+				delete d.onErr[event][p.opts.playerID];
+			});
+			    
 			delete d.onQualityChange[p.opts.playerID];
 			
 			delete $.tubeplayer.events[p.opts.playerID]; // flash callback ref's
