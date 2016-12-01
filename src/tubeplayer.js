e37220ee85dad6627e05bfdc44c2e381c7b500cc
Nirvana Tikku <ntikku@gmail.com>
2016-12-01 17:46:44 -0500

    Fix spacing - remove unnecessary lines and use curlies for if blocks

--- src/tubeplayer.js
+++ src/tubeplayer.js
@@ -1,31 +1,18 @@
 (function($) {
-
 	'use strict';
-
 	//
 	//  namespace
-	//
 	var TUBEPLAYER = ".tubeplayer",
-
 		TUBEPLAYER_CLASS = "jquery-youtube-tubeplayer",
-
 		OPTS = "opts" + TUBEPLAYER;
 
 	//	
-	//
 	// TubePlayer package 
-	//
-	//
 	var TP = {
-		
 		inited: false,				// tubeplayer inited flag - for destroy/re-init
-		
 		ytplayers: {},				// all the instances that exist
-		
 		inits: [],					// local init functions for multiple iframe players
-		
 		iframeScriptInited: false,	// no need to import the iframe script multiple times
-		
 		State: { 
 			'UNSTARTED': -1,
 			'ENDED': 0,
@@ -34,7 +21,6 @@
 			'BUFFERING': 3,
 			'CUED': 5
 		},
-		
 		Error: {
 			'BAD_INIT': 0,
 			'INVALID_PARAM': 2,
@@ -42,20 +28,13 @@
 			'NOT_EMBEDDABLE': 101,
 			'CANT_PLAY': 150
 		}
-		
 	};
 
 	//
-	//
 	// public facing defaults
-	//
-	//
 	$.tubeplayer = {
-		
 		events: {},				// events cache -- used by flashplayer version of video player
-		
 		TubePlayer: TP			// reference to the internal TubePlayer object. primarily exposed for testing.
-		
 	};
 
 	/**
@@ -67,103 +46,67 @@
 	 * own risk.
 	 */
 	$.tubeplayer.defaults = {
-
 		afterReady: function() {}, // args: $player
-
 		stateChange: function(player) {
-
 			var _ret = this.onPlayer;
-
 			return function(state) {
-
 				var _player = $('#'+player).parent();
-			
 				if (typeof(state) === "object") {
 					state = state.data;
 				}
-
 				switch (state) {
-
 				case TP.State.UNSTARTED:
 					return _ret.unstarted[player].call(_player);
-
 				case TP.State.ENDED:
 					return _ret.ended[player].call(_player);
-
 				case TP.State.PLAYING:
 					return _ret.playing[player].call(_player);
-
 				case TP.State.PAUSED:
 					return _ret.paused[player].call(_player);
-
 				case TP.State.BUFFERING:
 					return _ret.buffering[player].call(_player);
-
 				case TP.State.CUED:
 					return _ret.cued[player].call(_player);
-
 				default:
 					return null;
-
 				}
 			};
-
 		},
-
 		onError: function(player) {
-
 			var _ret = this.onErr;
-
 			return function(errorCode) {
-				
 				var _player = $('#'+player).parent();
-
 				if (typeof(errorCode) === "object") {
 					errorCode = errorCode.data;
 				}
-
 				switch (errorCode) {
+					case TP.Error.BAD_INIT:
+					case TP.Error.INVALID_PARAM:
+						return _ret.invalidParameter[player].call(_player);
 
-				case TP.Error.BAD_INIT:
-				case TP.Error.INVALID_PARAM:
-					return _ret.invalidParameter[player].call(_player);
+					case TP.Error.NOT_FOUND:
+						return _ret.notFound[player].call(_player);
 
-				case TP.Error.NOT_FOUND:
-					return _ret.notFound[player].call(_player);
+					case TP.Error.NOT_EMBEDDABLE:
+					case TP.Error.CANT_PLAY:
+						return _ret.notEmbeddable[player].call(_player);
 
-				case TP.Error.NOT_EMBEDDABLE:
-				case TP.Error.CANT_PLAY:
-					return _ret.notEmbeddable[player].call(_player);
-
-				default:
-					return _ret.defaultError[player].call(_player);
-
+					default:
+						return _ret.defaultError[player].call(_player);
 				}
-
 			};
-
 		},
-
 		qualityChange: function(player) {
-
 			var _this = this;
-
 			return function(suggested) {
-				
 				var _player = $('#'+player).parent();
-
 				if (typeof(suggested) === "object") {
 					suggested = suggested.data;
 				}
-
 				return _this.onQualityChange[player].call(_player, suggested);
-
 			};
-
 		},
-
 		onQualityChange: {},
-
 		onPlayer: {
 			unstarted: {},
 			ended: {},
@@ -172,14 +115,12 @@
 			buffering: {},
 			cued: {}
 		},
-
 		onErr: {
 			defaultError: {},
 			notFound: {},
 			notEmbeddable: {},
 			invalidParameter: {}
 		}
-
 	};
 
 	/**
@@ -256,23 +197,14 @@
 	 * as well as invoke events that are attached - typically getters/setters
 	 */
 	$.fn.tubeplayer = function(input, xtra) {
-
 		var $this = $(this);
-
 		var type = typeof input;
-
 		if (arguments.length === 0 || type === "object") {
-
 			return $this.each(function() {
-
 				TP.init($(this), input);
-
 			});
-
 		} else if (type === "string") {
-
 			return $this.triggerHandler(input + TUBEPLAYER, (typeof xtra !== 'undefined' ? xtra : null));
-		
 		}
 
 	};
@@ -283,36 +215,24 @@
 	 * that are bound to the TubePlayer.
 	 */
 	var wrap_fn = function(fn) {
-
 		return function(evt, param) {
-
 			var p = TP.getPkg(evt);
-
 			if (p.ytplayer) {
-
 				var ret = fn(evt, param, p);
-
 				if (typeof(ret) === "undefined") {
 					ret = p.$player;
 				}
-
 				return ret;
-
 			}
-
 			return p.$player;
-
 		};
-
 	};
 
 	/**
 	 * Public method to get all the player instances
 	 */
 	$.tubeplayer.getPlayers = function() {
-
 		return TP.ytplayers;
-
 	};
 
 
@@ -331,52 +251,34 @@
 	 *	@param opts - the user's options
 	 */
 	TP.init = function($player, opts) {
-
 		if ($player.hasClass(TUBEPLAYER_CLASS)) return $player;
-
 		var o = $.extend({}, defaults, opts);
-
 		o.playerID += "-" + guid();
-
 		$player.addClass(TUBEPLAYER_CLASS).data(OPTS, o);
-
-		for (var event in PLAYER)
+		for (var event in PLAYER){
 			$player.bind(event + TUBEPLAYER, $player, PLAYER[event]);
-
+		}
 		// initialize the default event methods
 		TP.initDefaults($.tubeplayer.defaults, o);
-
 		// insert the player container
 		$("<div></div>").attr("id", o.playerID).appendTo($player);
-
 		// append the player into the container
 		TP.initPlayer($player, o);
-
 		return $player;
-
 	};
 
 	/**
 	 * Every method needs these items
 	 */
 	TP.getPkg = function(evt) {
-
 		var $player = evt.data;
-
 		var opts = $player.data(OPTS);
-
 		var ytplayer = TP.ytplayers[opts.playerID];
-
 		return {
-
 			$player: $player,
-
 			opts: opts,
-
 			ytplayer: ytplayer
-
 		};
-
 	};
 
 	/**
@@ -388,101 +290,56 @@
 	 * pop the method off the queue and init the player.
 	 */
 	TP.iframeReady = function(o) {
-
 		TP.inits.push(function() {
-
 			new YT.Player(o.playerID, {
-
 				videoId: o.initialVideo,
-
 				width: o.width,
-
 				height: o.height,
-
 				playerVars: {
-
 					'autoplay': (o.autoPlay ? 1 : 0),
-
 					'autohide': (o.autoHide ? 1 : 0),
-
 					'controls': (o.showControls ? 1 : 0),
-
 					'loop': (o.loop ? 1 : 0),
-
 					'playlist': (o.loop ? o.initialVideo : ""),
-
 					'rel': (o.showRelated ? 1 : 0),
-
 					'fs': (o.allowFullScreen ? 1 : 0),
-
 					'wmode': o.wmode,
-
 					'showinfo': (o.showinfo ? 1 : 0),
-
 					'modestbranding': (o.modestbranding ? 1 : 0),
-
 					'iv_load_policy': (o.annotations ? 1 : 3),
-
 					'start': o.start,
-
 					'theme': o.theme,
-
 					'color': o.color,
-
 					'playsinline': o.playsinline
-
 				},
-
 				events: {
-
 					'onReady': function(evt) {
-
 						TP.ytplayers[o.playerID] = evt.target;
-
 						var $player = $(evt.target.getIframe()).parents("." + TUBEPLAYER_CLASS);
-
 						$.tubeplayer.defaults.afterReady($player);
-
 					},
-
 					'onPlaybackQualityChange': $.tubeplayer.defaults.qualityChange(o.playerID),
-
 					'onStateChange': $.tubeplayer.defaults.stateChange(o.playerID),
-
 					'onError': $.tubeplayer.defaults.onError(o.playerID)
-
 				}
-
 			});
-
 		});
 
 		// stacked init method
 		if (TP.inits.length >= 1 && !TP.inited) {
-
 			return function() {
-
 				for (var i = 0; i < TP.inits.length; i++) {
-
 					TP.inits[i]();
-
 				}
-
 				TP.inited = true;
-
 			};
-
 		}
 
 		// if we've inited already, just call the init fn
 		if (TP.inited) {
-
 			(TP.inits.pop())();
-
 		}
-
 		return window.onYouTubePlayerAPIReady;
-
 	};
 
 	/**
@@ -532,25 +389,16 @@
 	 * Initialize an iframe player
 	 */
 	TP.initIframePlayer = function($player, o) {
-
 		if (!TP.iframeScriptInited) {
-
 			// write the api script tag
 			var tag = document.createElement('script');
-
 			tag.src = o.protocol + "://www.youtube.com/iframe_api";
-
 			var firstScriptTag = document.getElementsByTagName('script')[0];
-
 			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 			TP.iframeScriptInited = true;
-
 		}
-
 		// init the iframe player
 		window.onYouTubePlayerAPIReady = TP.iframeReady(o);
-
 	};
 
 	/**
@@ -562,22 +410,15 @@
 	 * @imports swfobject automatically
 	 */
 	TP.initFlashPlayer = function($player, o) {
-
 		if (o.loadSWFObject) {
-
 			// cleanup swfobjectURL to re-apply the protocol
 			o.swfobjectURL = o.swfobjectURL.replace('http://', '');
 			o.swfobjectURL = o.swfobjectURL.replace('https://', '');
 			o.swfobjectURL = o.protocol + '://' + o.swfobjectURL;
-
 			$.getScript(o.swfobjectURL, TP.init_flash_player(o));
-
 		} else {
-
 			TP.init_flash_player(o)();
-
 		}
-
 	};
 
 	TP.init_flash_player = function(o) {
@@ -622,42 +463,29 @@
 
 			// init the player ready fn
 			window.onYouTubePlayerReady = function(playerId) {
-
 				var player = document.getElementById(playerId);
-
 				var pid = playerId.replace(/-/g, '');
-
 				var d = $.tubeplayer.defaults;
 				$.tubeplayer.events[pid] = {
 					"stateChange": d.stateChange(playerId),
 					"error": d.onError(playerId),
 					"qualityChange": d.qualityChange(playerId)
 				};
-
 				player.addEventListener("onStateChange", "$.tubeplayer.events." + pid + ".stateChange");
 				player.addEventListener("onError", "$.tubeplayer.events." + pid + ".error");
 				player.addEventListener("onPlaybackQualityChange", "$.tubeplayer.events." + pid + ".qualityChange");
-
 				TP.ytplayers[playerId] = player;
-
 				var $player = $(player).parents("." + TUBEPLAYER_CLASS);
-
 				$.tubeplayer.defaults.afterReady($player);
-
 			};
-
 		};
-
 	};
 
 	// fmt: youtube.com/watch?x=[anything]&v=[desired-token]&
 	TP.getVideoIDFromURL = function(sURL) {
-
 		sURL = sURL || ""; // make sure it's a string; sometimes the YT player API returns undefined, and then indexOf() below will fail
 		var qryParamsStart = sURL.indexOf("?");
-
 		var qryParams = sURL.substring(qryParamsStart, sURL.length);
-
 		var videoStart = qryParams.indexOf("v=");
 		if (videoStart > -1) {
 			var videoEnd = qryParams.indexOf("&", videoStart);
@@ -666,58 +494,40 @@
 			}
 			return qryParams.substring(videoStart + "v=".length, videoEnd);
 		}
-
 		return "";
-
 	};
 
 	/**
 	 * All the events that are bound to a TubePlayer instance
 	 */
 	var PLAYER = {
-		
 		opts: wrap_fn(function(evt,param,p){
-			
 			return p.opts;
-			
 		}),
-
 		cue: wrap_fn(function(evt, param, p) {
-
 			p.ytplayer.cueVideoById(param, 0, p.opts.preferredQuality);
-
 		}),
-
 		play: wrap_fn(function(evt, param, p) {
-
-			if (typeof(param) === 'object') p.ytplayer.loadVideoById({videoId: param.id, startSeconds: param.time, suggestedQuality: p.opts.preferredQuality });
-
-			else if (typeof param !== 'undefined') p.ytplayer.loadVideoById({videoId: param, startSeconds: 0, suggestedQuality: p.opts.preferredQuality });
-
-			else p.ytplayer.playVideo();
-
+			if (typeof(param) === 'object') {
+				p.ytplayer.loadVideoById({videoId: param.id, startSeconds: param.time, suggestedQuality: p.opts.preferredQuality });
+			}
+			else if (typeof param !== 'undefined') {
+				p.ytplayer.loadVideoById({videoId: param, startSeconds: 0, suggestedQuality: p.opts.preferredQuality });
+			}
+			else {
+				p.ytplayer.playVideo();
+			}
 			p.opts.onPlay(param);
-
 		}),
-
 		pause: wrap_fn(function(evt, param, p) {
-
 			p.ytplayer.pauseVideo();
-
 			p.opts.onPause(p);
-
 		}),
-
 		stop: wrap_fn(function(evt, param, p) {
-
 			p.ytplayer.stopVideo();
-
 			p.opts.onStop(p);
-
 		}),
-
 		seek: wrap_fn(function(evt, param, p) {
-
 			if (/:/.test(param)) {
 				var parts = param.split(":").reverse();
 				param = 0;
@@ -725,162 +535,88 @@
 					param += Math.pow(60, i) * (parts[i] | 0);
 				}
 			}
-
 			p.ytplayer.seekTo(param, true);
-
 			p.opts.onSeek(param);
-
 		}),
-
 		mute: wrap_fn(function(evt, param, p) {
-
 			p.$player.attr("data-prev-mute-volume", p.ytplayer.getVolume());
-
 			p.ytplayer.mute();
-
 			p.opts.onMute(p);
-
 		}),
-
 		unmute: wrap_fn(function(evt, param, p) {
-
 			p.ytplayer.unMute();
-
 			p.ytplayer.setVolume((p.$player.attr("data-prev-mute-volume") || 50));
-
 			p.opts.onUnMute();
-
 		}),
-
 		isMuted: wrap_fn(function(evt, param, p) {
-
 			return p.ytplayer.isMuted();
-
 		}),
-
 		volume: wrap_fn(function(evt, param, p) {
-
 			if (typeof param !== 'undefined') {
-
 				p.ytplayer.setVolume(param);
-
 				p.$player.attr("data-prev-mute-volume", p.ytplayer.getVolume());
-
 			} else {
-
 				return p.ytplayer.getVolume() || 0; // 0 because iframe's currently in labs
-				
 			}
-
 		}),
-
 		quality: wrap_fn(function(evt, param, p) {
-
 			if (typeof param !== 'undefined') p.ytplayer.setPlaybackQuality(param);
-
 			else return p.ytplayer.getPlaybackQuality();
-
 		}),
-		
 		playbackRate: wrap_fn(function(evt, param, p){
-			
 			if(typeof param !== "undefined") p.ytplayer.setPlaybackRate(param);
-		
 			else return p.ytplayer.getPlaybackRate();
-			
 		}),
-
 		data: wrap_fn(function(evt, param, p) {
-
 			var ret = {};
-
 			var P = p.ytplayer;
-
 			ret.videoLoadedFraction = P.getVideoLoadedFraction();
-
 			ret.bytesLoaded = P.getVideoBytesLoaded(); // deprecated
-
 			ret.bytesTotal = P.getVideoBytesTotal(); // deprecated
-
 			ret.startBytes = P.getVideoStartBytes(); // deprecated
-
 			ret.state = P.getPlayerState();
-
 			ret.currentTime = P.getCurrentTime();
-
 			ret.duration = P.getDuration();
-
 			ret.videoURL = P.getVideoUrl();
-
 			ret.videoEmbedCode = P.getVideoEmbedCode();
-
 			ret.videoID = TP.getVideoIDFromURL(ret.videoURL);
-
 			ret.availableQualityLevels = P.getAvailableQualityLevels();
-			
 			ret.availablePlaybackRates = P.getAvailablePlaybackRates();
-
 			return ret;
-
 		}),
-
 		videoId: wrap_fn(function(evt, param, p) {
-
 			return TP.getVideoIDFromURL(p.ytplayer.getVideoUrl());
-
 		}),
-
 		size: wrap_fn(function(evt, param, p) {
-
 			if (typeof param !== 'undefined' && param.width && param.height) {
-
 				p.ytplayer.setSize(param.width, param.height);
-
 				$(p.ytplayer).css(param);
-
 			}
-
 		}),
-
 		destroy: wrap_fn(function(evt, param, p) {
-			
 			p.$player.removeClass(TUBEPLAYER_CLASS).data(OPTS, null).unbind(TUBEPLAYER).html("");
-
 			delete TP.ytplayers[p.opts.playerID];
-
 			// cleanup callback handler references..
 			var d = $.tubeplayer.defaults;
-
 			var events = ['unstarted', 'ended', 'playing', 'paused', 'buffering', 'cued'];
-
 			$.each(events, function(i, event) {
 				delete d.onPlayer[event][p.opts.playerID];
 			});
-
 			events = ['defaultError', 'notFound', 'notEmbeddable', 'invalidParameter'];
 			$.each(events, function(i, event) {
 				delete d.onErr[event][p.opts.playerID];
 			});
-
 			delete d.onQualityChange[p.opts.playerID];
-
 			delete $.tubeplayer.events[p.opts.playerID]; // flash callback ref's
 			if ('destroy' in p.ytplayer) {
 				p.ytplayer.destroy();
 			}
-
 			$(p.ytplayer).remove();
-
 			return null;
-
 		}),
-
 		player: wrap_fn(function(evt, param, p) {
-
 			return p.ytplayer;
-
 		})
-
 	};
 
 	// used in case of multiple players
